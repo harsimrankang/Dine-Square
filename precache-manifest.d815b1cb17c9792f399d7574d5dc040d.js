bb6d96aa4adf312a36be73bf0dae68868ff4af23
harsimrankang <kang.harsimran1999gmail.com>
2020-09-18 23:12:13 +0530

    Updates

---
+++ precache-manifest.d815b1cb17c9792f399d7574d5dc040d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de27bd5f07977753d1293d2b7b73f608",
+    "url": "/Dine-Square/./index.html"
+  },
+  {
+    "revision": "5fa3679fdc86539c56da",
+    "url": "/Dine-Square/./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a90fba726ed2efad3df7",
+    "url": "/Dine-Square/./static/css/main.551216be.chunk.css"
+  },
+  {
+    "revision": "5fa3679fdc86539c56da",
+    "url": "/Dine-Square/./static/js/2.5ae5bc48.chunk.js"
+  },
+  {
+    "revision": "b9bf62f5644947be677f45bff7ad1106",
+    "url": "/Dine-Square/./static/js/2.5ae5bc48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a90fba726ed2efad3df7",
+    "url": "/Dine-Square/./static/js/main.32bb0890.chunk.js"
+  },
+  {
+    "revision": "f5363b5c01b32fc5a14b",
+    "url": "/Dine-Square/./static/js/runtime-main.3c47c42d.js"
+  },
+  {
+    "revision": "5c2ddbcde33f3cfa2e7dfaff611a577d",
+    "url": "/Dine-Square/./static/media/mainvid.5c2ddbcd.mp4"
+  },
+  {
+    "revision": "4241bfd3377b347db40d4594ade16c57",
+    "url": "/Dine-Square/./static/media/newbg.4241bfd3.jpg"
+  }
+]);
